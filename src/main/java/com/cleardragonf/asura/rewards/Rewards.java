96be5cb09963c1ead4827337eb2fbfbf9c11ddfb
chrism339@hotmail.com <chrism339@hotmail.com>
2024-08-06 14:46:19 -0500

    • Added PVP and Started work on NAtural Deaths to as repayments...

--- src/main/java/com/cleardragonf/asura/rewards/Rewards.java
+++ src/main/java/com/cleardragonf/asura/rewards/Rewards.java
@@ -8,13 +8,16 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
 
+
 import java.math.BigDecimal;
 import java.util.Map;
 
+import static com.cleardragonf.asura.HOB.economyManager;
 import static com.cleardragonf.asura.utilities.Formatting.formatAsCurrency;
 
 @Mod.EventBusSubscriber(modid = HOB.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
@@ -53,6 +56,34 @@ public class Rewards {
                     player.sendSystemMessage(Component.literal("You received " + formattedReward + " for killing a " + entityType + "!"));
                 }
             }
+        }
+
+        else if (source.getEntity() instanceof Player && entity instanceof Player) {
+            Player killer = (Player) source.getEntity();
+            Player victim = (Player) entity;
+
+            double vicitimsBalance = economyManager.getBalance(victim.getName().toString());
+            if (vicitimsBalance <= 0){
+                vicitimsBalance = 2.0;
+            }
+            double halfOfVictimsBalnce = vicitimsBalance / 2;
+            if(vicitimsBalance <= 0){
+
+            }else{
+                economyManager.subtractBalance(victim.getName().toString(),halfOfVictimsBalnce);
+            }
+            economyManager.addBalance(killer.getName().toString(),halfOfVictimsBalnce);
+            String formattedReward = formatAsCurrency(BigDecimal.valueOf(halfOfVictimsBalnce));
+            killer.sendSystemMessage(Component.literal("You received " + formattedReward + " for killing " + victim.getName().getString() + "!"));
+            victim.sendSystemMessage(Component.literal("You have lost " + formattedReward + " because you were killed by " + killer.getName().getString()));
+        }
+
+        //Player just dies...
+        else if(entity instanceof Player){
+            Player victim = (Player) entity;
+
+            double victimsBalance = economyManager.getBalance(victim.getName().toString());
+
         }
     }
 }
